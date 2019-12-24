7739d620a68e4debab6da80697692c14eb3f1a28
tawatchaiaem <tawatchai.h1994@gmail.com>
2019-12-24 21:59:47 +0700

    config

--- routes/backend/login.route.js
+++ routes/backend/login.route.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var config = require('../../config/config');
+var config = require('config/');
+let vat = config.get('vat');
 
 /* GET login page */
 router.get('/',function(req,res,next){
-    // console.log(process.env.NODE_ENV);
     
-    res.render('backend_view/login',{env:process.env.NODE_ENV});
+    res.render('backend_view/login',{env:process.env.NODE_ENV,vat:vat});
 });
 module.exports = router;
