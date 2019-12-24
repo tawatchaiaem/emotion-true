451dcabc0ad183a5c5108df7d98d9d72d7149179
tawatchaiaem <tawatchai.h1994@gmail.com>
2019-12-24 23:44:14 +0700

    controller login

--- routes/backend/login.route.js
+++ routes/backend/login.route.js
@@ -3,5 +3,6 @@ var router = express.Router();
 
 var login_controller = require('../../controllers/login.controller')
 /* GET login page */
-router.get('/',login_controller.login_view);
+router.get('/login',login_controller.login_view);
+router.post('/auth',login_controller.login_view);
 module.exports = router;
