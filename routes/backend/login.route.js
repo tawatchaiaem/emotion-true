86deff760d150784da93c802197337beef1fea3e
tawatchaiaem <tawatchai.h1994@gmail.com>
2019-12-24 10:33:44 +0700

    d

--- routes/backend/login.route.js
+++ routes/backend/login.route.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var router = express.Router()
-//var config = require('config');
+var config = require('../../config/config');
 
 /* GET login page */
 router.get('/',function(req,res,next){
-    //console.log(config);
+    console.log(process.env.NODE_ENV);
     res.render('backend_view/login');
 })
 module.exports = router
