3eb06cbbf8cebfc323f8a79106a0a16e3b22802a
tawatchaiaem <tawatchai.h1994@gmail.com>
2019-12-24 22:56:33 +0700

    update login favicon

--- routes/backend/login.route.js
+++ routes/backend/login.route.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 var config = require('config/');
 var base_url = config.get('base_url');
-
+var favicon = config.get('favicon');
+var environment = config.get('environment');
 /* GET login page */
 router.get('/',function(req,res,next){
-    console.log(base_url)
-    res.render('backend_view/login',{base_url:base_url});
+    console.log(environment)
+    var title;
+    if(environment == 'testing'){
+        title = "test emontion true";
+    }else{
+        title = "login emotion true";
+    }
+    res.render('backend_view/login',{title:title,base_url:base_url,favicon:favicon});
 });
 module.exports = router;
