5f57db938b915950b9b5d5b8004afed1ec11a140
tawatchaiaem <tawatchai.h1994@gmail.com>
2019-12-24 16:19:33 +0700

    edit config base url

--- routes/backend/login.route.js
+++ routes/backend/login.route.js
@@ -1,10 +1,11 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 var config = require('../../config/config');
 
 /* GET login page */
 router.get('/',function(req,res,next){
-    console.log(process.env.NODE_ENV);
+    // console.log(process.env.NODE_ENV);
+    
     res.render('backend_view/login',{env:process.env.NODE_ENV});
-})
-module.exports = router
+});
+module.exports = router;
